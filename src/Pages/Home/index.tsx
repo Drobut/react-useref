737ab255b6a991f0e9e64530fa25443de518e8ca
Diogo Matias <diogomaccedo200@gmail.com>
2022-06-29 20:52:52 -0300

    teste1

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,22 +1,22 @@
-import {useNavigate} from 'react-router-dom'
-import './styles.css';
-
+import { useNavigate } from "react-router-dom";
+import "./styles.css";
 
 export default function Home() {
-   
-    let navigate = useNavigate();
-    function handleClick() {
-      navigate('/explication')
-    }
+  let navigate = useNavigate();
+  function handleClick() {
+    navigate("/explication");
+  }
 
-    return(
-        <>  
-            <div className='page'>
-            <div className="Icon"></div>
-            <h1>React useRef</h1>
-            <button className='btn1' onClick={handleClick}>Next</button>
-            </div>
-        </>
-    )
-    
+  return (
+    <>
+      <h1>Teste</h1>
+      <div className="page">
+        <div className="Icon"></div>
+        <h1>React useRef</h1>
+        <button className="btn1" onClick={handleClick}>
+          Next
+        </button>
+      </div>
+    </>
+  );
 }
