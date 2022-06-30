b90556d8e218448456e6ed434f79d21d3cbab948
rafael-correa98 <rafaelcorrean98@gmail.com>
2022-06-29 21:28:07 -0300

    test

--- src/Pages/Explication/index.tsx
+++ src/Pages/Explication/index.tsx
@@ -1,5 +1,6 @@
 import './styles.css';
 import {useNavigate} from 'react-router-dom'
+import { Typography } from '@mui/material';
 
 export default function Explication() {
     
@@ -10,11 +11,39 @@ export default function Explication() {
 
 
     return(
+      <>
         <div className='page'>
         <div className="Icon"></div>
         <h1>Explication</h1>
+        <Typography variant="h1" component="div" gutterBottom>
+                useRef
+            </Typography>
+            <Typography variant="body1">
+            A partir da versão 16.8 do React é possível usar os Hooks como uma nova maneira de escrever componentes e reutilizar o código entre eles. Um desses hooks é o useRef. O useRef atua como uma função que retorna um objeto ref e recebe um argumento que inicializa a propriedade .current desse objeto.
+
+            Estrutura: 
+
+            const refContainer = useRef(initialValue);
+
+
+            O useRef é uma alternativa para o useState e ter um front-end com mais performace e também para fazer referência a elementos na arvóre de elementos
+
+            useRef utilizar um valor de referência, ele pode ser usado para armazenar um valor mutável que não causa uma nova renderização quando atualizado. Ele pode ser usado para acessar diretamente um elemento DOM.
+
+            Ele dar mais performace a aplicação evitando renderizações desnecessárias.
+
+
+            Segundo a documentação do React, existem algumas boas finalidades para o uso de refs:
+
+            - Gerenciamento de foco, seleção de texto, ou reprodução de mídia.
+            - Engatilhar animações imperativas.
+            - Integração com bibliotecas DOM de terceiros.
+
+            Podemos usar o useRef para essas finalidades. Uma outra aplicação é poder acessar as props ou o estado anterior de um componente funcional.
+            </Typography>
         <button className='btn1' onClick={handleClick}>Next</button>
         <button className='btn2' onClick={() => navigate(-1)}>Back</button>
         </div>
+      </>
     )
 }
